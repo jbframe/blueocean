65304fd91934aa032dd245a7988dafebd1c68745
Hayden West <haydenw7@gmail.com>
2021-04-02 14:42:11 -0700

    set up express server to listen on port 3000

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = 3000;
-
-app.use(express.static(path.join(__dirname, "/../pages")));
 
 // ROUTE IMPORTS
 

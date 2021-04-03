45d255a3744bb3265138038d947a3cd6da566002
Hayden West <haydenw7@gmail.com>
2021-04-02 19:17:34 -0700

    merged with testing branch

--- components/home/EventCard.js
+++ components/home/EventCard.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const EventCard = ({ image, name, location, date }) => (
-
   // using placeholder image
   // final images will be passed down as props
   // when mapped over
 
   <div>
-    <img className="event-card-img" src="../../public/event-card-placeholder.png" alt="event card cover" />
+    <img
+      className="event-card-img"
+      src="../../public/event-card-placeholder.png"
+      alt="event card cover"
+    />
     <div className="event-card-name">{name}</div>
     <div className="event-card-location">{location}</div>
     <div className="event-card-date">{date}</div>
   </div>
 );
+
+export default EventCard;

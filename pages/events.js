c0126b3b5256d35aaaf41b93cd308b51b5de0683
Hayden West <haydenw7@gmail.com>
2021-04-02 18:03:05 -0700

    craeted EventList component for Event's page

--- pages/events.js
+++ pages/events.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+import EventsList from "../components/home/EventsList";
+
 export default function Events() {
   const [events, setEvents] = useState([]);
 
@@ -17,7 +19,10 @@ export default function Events() {
         <title>Events</title>
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <h1>UPCOMING EVENTS</h1>
+        <EventsList events={events} />
+      </main>
 
       <footer className={styles.footer}></footer>
     </div>

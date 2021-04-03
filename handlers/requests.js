78bcdbb5e9ea1955142e372a214a9b5bc9b112dc
Hayden West <haydenw7@gmail.com>
2021-04-02 19:27:48 -0700

    Created Requests.js inside handlers folder for API requests to the server

--- handlers/requests.js
+++ handlers/requests.js
@@ -1,5 +1,34 @@
 const axios = require("axios");
 
-const requests = {};
+const server = "SERVER GOES HERE";
+
+const requests = {
+  // Fetch User Events
+  fetchUserEvents(user, cb) {
+    axios
+      .get(`${server}/ENDPOINT GOES HERE`)
+      .then((response) => {
+        cb(response.data);
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+        console.log(err);
+      });
+  },
+
+  // Fetch All Events
+  fetchAllEvents(cb) {
+    axios.
+      .get(`${server}/ENDPOINT GOES HERE`)
+      .then((response) => {
+        cb(response.data);
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+        console.log(err);
+      })
+  },
+
+};
 
 module.exports = requests;

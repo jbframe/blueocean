140976b0147b65f222e2b40051de4e1049a260d4
Hayden West <haydenw7@gmail.com>
2021-04-03 11:46:37 -0700

    Cleaned up merge conflicts

--- handlers/requests.js
+++ handlers/requests.js
@@ -1,18 +1,12 @@
 const axios = require("axios");
 
 const server = "http://localhost:4000";
-const config = {
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  },
-};
 
 const requests = {
   // Fetch User Events
   fetchUserEvents(user, cb) {
     axios
-      .get(`${server}/user/${user}`, config)
+      .get(`${server}/user/${user}`)
       .then((response) => {
         cb(response.data);
       })

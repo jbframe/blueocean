66b61b079c17cf8c7f7842302cff08f304629cfe
Jim Burch <jrburch1@gmail.com>
2021-04-02 23:45:03 -0700

    built navbar shell

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import TextField from '@material-ui/core/TextField';
+import EventAvailableIcon from '@material-ui/icons/EventAvailable';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
 
   return (
-    <div>
-      <span>Navbar</span>
+    <div className={styles.navbar}>
+      <EventAvailableIcon style={{ fontSize: 50 }} />
+      <TextField id="search-events-bar" className="search-events-bar" label="Search Events" variant="outlined" />
+      <span className={styles.create}>Create Event</span>
+      <span className={styles.my_events}>My Events</span>
+      <AccountCircleIcon style={{ fontSize: 50 }} />
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,17 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+import Layout from '../components/Layout';
 import EventsList from "../components/home/EventsList";
 
-const requests = require("../handlers/requests");
+// const requests = require("../handlers/requests");
 
 export default function Home() {
   const [userEvents, setUserEvents] = useState([]);
   const [allEvents, setAllEvents] = useState([]);
 
+  // COMMENTING OUT WHILE SETTING UP NAV AND LAYOUT
+  /*
   useEffect(() => {
     // Preform fetch request for Events Data
     // update events to pass into both eventsLists
@@ -23,25 +26,30 @@ export default function Home() {
       console.log(data);
     })
   });
+  */
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>My Dashboard</title>
-      </Head>
+    // Wrap every page component in <Layout> tags (and import up top)
+    // to have the nav bar up top
+    <Layout>
+      <div className={styles.container}>
+        <Head>
+          <title>My Dashboard</title>
+        </Head>
 
-      <main className={styles.main}>
-        <div>
-          <h5>My Events</h5>
-          <EventsList events={userEvents} />
-        </div>
-        <div>
-          <h5>All Events</h5>
-          <EventsList events={allEvents} />
-        </div>
-      </main>
+        <main className={styles.main}>
+          <div>
+            <h5>My Events</h5>
+            <EventsList events={userEvents} />
+          </div>
+          <div>
+            <h5>All Events</h5>
+            <EventsList events={allEvents} />
+          </div>
+        </main>
 
-      <footer className={styles.footer}></footer>
-    </div>
+        <footer className={styles.footer}></footer>
+      </div>
+    </Layout>
   );
 }

d045a777f7c0bda3553a403df2651b0c2e6a0d70
21grahams <graham.kirsh@gmail.com>
2021-04-05 17:22:31 -0600

    Create Events modal created and functional. Still needs validation and Post Requst hookup for Submit Button

--- pages/createEvent.js
+++ pages/createEvent.js
@@ -1,2 +1,112 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Image } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+function CreateEvent(props) {
+  const [eventName, setEventName] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
+  const [eventLocation, setEventLocation] = useState("");
+  const [maxAttendees, setMaxAttendees] = useState(0);
+  const [photos, setPhotos] = useState(null);
+  let photoDisplay = [];
+
+  const clearFields = () => {
+    setEventName("");
+    setEventDescription("");
+    setEventLocation("");
+    setMaxAttendees(0);
+    setPhotos("");
+  };
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    console.log("SUBMITTED");
+    clearFields();
+  };
+
+  const configurePhoto = () => {
+    setPhotos(URL.createObjectURL(event.target.files[0]));
+    console.log(URL.createObjectURL(event.target.files[0]));
+    console.log(event.target.files[0]);
+  };
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Enter Event Details...
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <input
+          value={eventName}
+          onChange={(e) => setEventName(e.target.value)}
+          placeholder="Event Name"
+        ></input>
+        <br></br>
+
+        <textarea
+          value={eventDescription}
+          onChange={(e) => setEventDescription(e.target.value)}
+          placeholder="Event Description"
+        ></textarea>
+        <br></br>
+
+        <input
+          value={eventLocation}
+          onChange={(e) => setEventLocation(e.target.value)}
+          placeholder="Add Event Location"
+        ></input>
+        <br></br>
+
+        <label>
+          Max Attendees
+          <select
+            value={maxAttendees}
+            onChange={(e) => setMaxAttendees(e.target.value)}
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+            <option>13</option>
+            <option>14</option>
+            <option>15</option>
+            <option>16</option>
+            <option>17</option>
+            <option>18</option>
+            <option>19</option>
+            <option>20</option>
+          </select>
+        </label>
+        <br></br>
+
+        <label>
+          Photos
+          <input type="file" onChange={configurePhoto}></input>
+          <img src={photos} />
+        </label>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.onHide}>Close</Button>
+        <Button onClick={handleSubmit}>Submit</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default CreateEvent;
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from "react";
-
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-
+import CreateEvent from "./CreateEvent";
 import EventsList from "../components/home/EventsList";
+import { Button, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const requests = require("../handlers/requests");
 
 export default function Home() {
   const [userEvents, setUserEvents] = useState([]);
   const [allEvents, setAllEvents] = useState([]);
+  const [modalShow, setModalShow] = React.useState(false);
 
   useEffect(() => {
     // Preform fetch request for Events Data
     // update events to pass into both eventsLists
 
-    requests.fetchUserEvents('USERNAME GOES HERE', (data) => {
+    requests.fetchUserEvents("USERNAME GOES HERE", (data) => {
       console.log(data);
-    })
+    });
 
     requests.fetchAllEvents((data) => {
       console.log(data);
-    })
+    });
   });
+
+  const handleModal = () => {
+    !setModal;
+  };
 
   return (
     <div className={styles.container}>
@@ -39,8 +45,15 @@ export default function Home() {
           <h5>All Events</h5>
           <EventsList events={allEvents} />
         </div>
+        <div>
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            Create Event
+          </Button>
+          <CreateEvent
+            show={modalShow}
+            onHide={() => setModalShow(false)}/>
+        </div>
       </main>
-
       <footer className={styles.footer}></footer>
     </div>
   );

a6ab11fa7094842b4d222efd010964fc2476268f
Dennis Arnold <dennis.j.arnold.iii@gmail.com>
2021-04-06 13:54:54 -0600

    implemented put and post requests

--- server/routes/addToEvent.js
+++ server/routes/addToEvent.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const signUpRouter = express.Router();
+const queries = require("../../database/queries");
 
 signUpRouter.use(express.json());
 
@@ -7,7 +8,13 @@ signUpRouter.put("/", (req, res) => {
   const user_id = req.body.user_id;
   const event_id = req.body.event_id;
 
-  // DATABASE UPDATE GOES HERE
+  queries.makeUserAnAttendee(user_id, event_id, (err, results) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      res.send(results);
+    }
+  })
 
   res.sendStatus(200);
 });
--- server/routes/allEvents.js
+++ server/routes/allEvents.js
@@ -14,4 +14,38 @@ eventsRouter.get("/", (req, res) => {
   });
 });
 
+// req.body.event = { name, location, date, hostId, meetingUrl, summary, max }
+eventsRouter.post("/create", (req, res) => {
+  let event = req.body.event
+  queries.insertEvent(event, (err, results) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      res.send(results);
+    }
+  })
+})
+
+// req.body.questions = [
+//   {
+//     text: "Question text here?",
+//     answers: [
+//       {
+//         text: "answer text here.",
+//         correct: Boolean
+//       },
+//       {}, {}...
+//     ]
+//   },
+//   {}, {}...
+// ]
+eventsRouter.Router.post("/assessment/:id", (req, res) => {
+  queries.insertAssessment(req.params.id, req.body.questions, (err, results) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      res.send(results);
+    }
+  })
+})
 module.exports = eventsRouter;
